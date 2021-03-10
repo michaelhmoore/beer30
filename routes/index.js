76d3d98e84cc7d6a2161b579d09921ebbf88e053
Andrew Cochran <acoch22@gmail.com>
2021-03-10 12:24:05 -0500

    added templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const app = require('../app');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Beer30' });
 });
+
+router.get('/')
 
 module.exports = router;
