4e20a8f1fd561d5ef985be12bd6be8966dd22608
michael moore <michael.moore@hey.com>
2021-03-16 12:39:27 -0400

    updated ssl:true config

--- config/config.js
+++ config/config.js
@@ -9,6 +9,9 @@ module.exports =
     "dialect": "postgres"
   },
   "production": {
-    "use_env_variable": "DATABASE_URL"
+    "use_env_variable": "DATABASE_URL",
+    "dialectOptions": {
+      ssl:true
+    }
   }
 }
